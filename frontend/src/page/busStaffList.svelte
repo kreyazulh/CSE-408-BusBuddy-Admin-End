54f36d6176038ff03c498dc4b5d52b8402462081
kreyazulh <1905082@ugrad.cse.buet.ac.bd>
2024-01-25 21:18:44 +0600

    staff profile

--- frontend/src/page/busStaffList.svelte
+++ frontend/src/page/busStaffList.svelte
@@ -1,10 +1,8 @@
-<!-- App.svelte -->
 <script>
   import { onMount } from 'svelte';
   import Navbar from './navbar.svelte';
   import { checkSession, isAuthenticated } from '../auth';
   import { navigate } from 'svelte-routing';
-
 
   let bus_staffs = [];
 
@@ -21,39 +19,36 @@
 </script>
 
 {#if $isAuthenticated}
-<main class="flex">
-  <div class="w-1/4">
-    <Navbar />
-</div>
-<div class="mx-auto max-w-md">
-  <h1>Bus Staff List</h1>
-  
-  {#if bus_staffs.length > 0}
-        {#each bus_staffs as {id, name, department,designation,residence,phone}}
-          <p>{id},{name},{department},{designation},{residence},{phone}</p>
+  <main class="flex">
+    <div class="w-fit">
+      <Navbar />
+    </div>
+    <div class="mx-auto max-w-lg">
+      <h1 class="text-4xl font-bold mb-4 text-blue-600">Bus Staff List</h1>
+      
+      {#if bus_staffs.length > 0}
+        {#each bus_staffs as { id, name, department, designation, residence, phone }}
+          <div class="bg-gray-100 rounded-md p-4 mb-4">
+            <div class="flex items-center justify-between mb-2">
+              <p class="text-lg font-semibold">ID: {id}</p>
+              <p class="text-lg font-semibold">Name: {name}</p>
+            </div>
+            <div class="flex items-center justify-between mb-2">
+              <p class="text-gray-600">Department: {department}</p>
+              <p class="text-gray-600">Designation: {designation}</p>
+            </div>
+            <div class="flex items-center justify-between mb-2">
+              <p class="text-gray-600">Residence: {residence}</p>
+              <p class="text-gray-600">Phone: {phone}</p>
+            </div>
+          </div>
         {/each}
-  {:else}
-    <p>You need to login first</p>
-    <a href="/login">Log In</a>
-  {/if}
-</main>
-
-<style>
-  main {
-    text-align: center;
-    margin: 2em;
-  }
-
-  table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 1em;
-  }
-
-  th, td {
-    border: 1px solid #ddd;
-    padding: 8px;
-    text-align: left;
-  }
-</style>
+      {:else}
+        <div class="bg-gray-100 rounded-md p-4 mb-4">
+          <p class="text-gray-800">You need to login first</p>
+          <a href="/login" class="text-blue-500 underline">Log In</a>
+        </div>
+      {/if}
+    </div>
+  </main>
 {/if}
