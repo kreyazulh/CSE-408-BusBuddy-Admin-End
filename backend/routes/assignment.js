be7ddf25805e675614869add7989c8829286c96f
mahabhu <mashroor184@gmail.com>
2024-03-03 19:41:06 +0600

    sched

--- backend/routes/assignment.js
+++ backend/routes/assignment.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // usage : createRoute
 router.get('/', (req, res) => {
     const client = req.client;
-    let query = 'SELECT * FROM assignment';
+    let query = 'SELECT * FROM schedule';
 
   
     client.query(query, (error, results) => {
@@ -18,12 +18,12 @@ router.get('/', (req, res) => {
     });
   });
 
-  router.post('/add', (req, res) => {
+  router.post('/update', (req, res) => {
     const client = req.client;
       console.log(req.body);
       client.query (
-          "INSERT INTO assignment (route, bus, driver, helper, start_time, shift) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP, $5);",
-          [req.body.route, req.body.bus, req.body.driver, req.body.helper, req.body.shift]
+          "UPDATE schedule SET default_driver = $3,default_helper = $4,default_bus = $5 WHERE route = $1 AND time_type = $2;",
+          [req.body.route, req.body.shift, req.body.driver, req.body.helper, req.body.bus]
       ).then(qres => {
           console.log(qres);
           if (qres.rowCount === 1) res.send(true);
@@ -36,7 +36,7 @@ router.get('/', (req, res) => {
 
   router.get('/allocatedBuses', (req, res) => {
     const client = req.client;
-    let query = 'SELECT * FROM bus WHERE reg_id IN (SELECT bus FROM assignment);';
+    let query = 'SELECT * FROM bus WHERE reg_id IN (SELECT default_bus FROM schedule);';
 
   
     client.query(query, (error, results) => {
@@ -51,7 +51,7 @@ router.get('/', (req, res) => {
 
   router.get('/unallocatedBuses', (req, res) => {
     const client = req.client;
-    let query = 'SELECT * FROM bus WHERE reg_id NOT IN (SELECT bus FROM assignment);';
+    let query = 'SELECT * FROM bus WHERE reg_id NOT IN (SELECT default_bus FROM schedule);';
 
   
     client.query(query, (error, results) => {
@@ -66,7 +66,7 @@ router.get('/', (req, res) => {
 
   router.get('/allocatedDrivers', (req, res) => {
     const client = req.client;
-    let query = `SELECT * FROM bus_staff WHERE id IN (SELECT driver FROM assignment) AND role = 'driver'`;
+    let query = `SELECT * FROM bus_staff WHERE id IN (SELECT default_driver FROM schedule) AND role = 'driver'`;
 
   
     client.query(query, (error, results) => {
@@ -81,7 +81,7 @@ router.get('/', (req, res) => {
 
   router.get('/unallocatedDrivers', (req, res) => {
     const client = req.client;
-    let query = `SELECT * FROM bus_staff WHERE id NOT IN (SELECT driver FROM assignment) AND role = 'driver'`;
+    let query = `SELECT * FROM bus_staff WHERE id NOT IN (SELECT default_driver FROM schedule) AND role = 'driver'`;
 
   
     client.query(query, (error, results) => {
@@ -96,7 +96,7 @@ router.get('/', (req, res) => {
 
   router.get('/allocatedHelpers', (req, res) => {
     const client = req.client;
-    let query = `SELECT * FROM bus_staff WHERE id IN (SELECT helper FROM assignment) AND role = 'collector'`;
+    let query = `SELECT * FROM bus_staff WHERE id IN (SELECT default_helper FROM schedule) AND role = 'collector'`;
 
   
     client.query(query, (error, results) => {
@@ -111,7 +111,7 @@ router.get('/', (req, res) => {
 
   router.get('/unallocatedHelpers', (req, res) => {
     const client = req.client;
-    let query = `SELECT * FROM bus_staff WHERE id NOT IN (SELECT helper FROM assignment) AND role = 'collector'`;
+    let query = `SELECT * FROM bus_staff WHERE id NOT IN (SELECT default_helper FROM schedule) AND role = 'collector'`;
 
   
     client.query(query, (error, results) => {
