6f58024a526bf17936123d14f5f7bf76bab7facd
MubasshiraMusarrat <1905088@ugrad.cse.buet.ac.bd>
2024-04-22 18:20:15 +0600

    can't remember what

--- frontend/src/page/TripPages/upcomingTrips.svelte
+++ frontend/src/page/TripPages/upcomingTrips.svelte
@@ -218,6 +218,7 @@ async function handleDeleteConfirm() {
           const result = await response.json();
           if (response.ok) {
             console.log("Row saved successfully:", result);
+            changedRows = changedRows.filter((r) => r !== id);
             await sendNotification(rowData);
             // Additional logic after successful save (e.g., update UI, clear fields)
           } else {
@@ -230,7 +231,6 @@ async function handleDeleteConfirm() {
         }
       }
       // Remove the id from changedRows after saving
-      changedRows = changedRows.filter((r) => r !== id);
     }
     await fetchRows(selectedDate);
   }
